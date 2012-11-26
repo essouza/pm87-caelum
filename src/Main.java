184e12c4470193885358ec21772d61ed8b1219e2
Erica <pm3013@caelum-Satellite-L305.(none)>
2012-11-26 21:06:57 -0200

    blergh

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Exemplo de teste de uso do git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
